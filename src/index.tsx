44c00746cc9995cd934cda10862628bcb05c15b6
itsryancooke <ryanjcooke07@gmail.com>
2026-02-19 16:17:28 +0000

    Sizing fixes

--- src/index.tsx
+++ src/index.tsx
@@ -4,65 +4,75 @@ import { type FC } from 'react'
 
 import { Retool } from '@tryretool/custom-component-support'
 
-export const IconInfo: FC = () => {
+export const ImageIcon: FC = () => {
+
+  // Component Variables
   const [name, _setName] = Retool.useStateString({
     name: 'name'
   })
-  // filename for background image
   const [filename, _setFileName] = Retool.useStateString({
-    name: 'filename'
-
+    name: 'filename',
+    initialValue: "https://purepng.com/public/uploads/large/purepng.com-messages-iconsymbolsiconsapple-iosiosios-8-iconsios-8-721522596080zgne4.png"
   })
-  // colour for circle
   const [circleColour, _setCircleColour] = Retool.useStateString({
     name: 'circle hex value',
     initialValue: "#ff4d4d"
 
   })
-  // text for inside circle
   const [count, _setCount] = Retool.useStateNumber({
     name: 'count',
     initialValue: 5
   })
+  // Click Event
+  const onClick = Retool.useEventCallback({name: "click"})
 
   return (
-    <div style={{aspectRatio: "1 / 1"}}>
+    <div style={{
+      overflow: "hidden",
+      width: "100%",
+      height: "100%",
+      alignItems: "center",
+    }}>
       <div style={{
         // Containing image to container
         width: "100%",
         height: "100%",
         overflow: "hidden",
       }}>
-        <img src={filename} alt="Sample Image" style={{
+        <img src={filename} onClick={() => console.log("Image clicked!")} alt="Sample Image" style={{
           position: "absolute",
           top: 0,
           left: 0,
           width: "100%",
           height: "100%",
           objectFit: "contain",
+          overflow: "hidden",
           zIndex: 10
         }}></img>
       
         <div
           style={{
-            aspectRatio: "1 / 1",
-            width: "30%",
+            width: "20%",
             height: "30%",
             borderRadius: "50%",
             backgroundColor: circleColour,     // color of the circle
             color: "white",                 // text color
             fontWeight: "bold",
-            fontSize: "100%",
             display: "flex",
             alignItems: "center",
             justifyContent: "center",
             position: "absolute",
-            top: "0.2em",
-            right: "0.2em",                    
+            overflow: "hidden",
+            top: 0,
+            right: "10%",                    
             zIndex: 20
-          }}
-        >
-        {count}
+          }}>
+          <div style={{
+            aspectRatio: "1 / 1",
+            fontSize: "1.5em",
+          }}>
+            {count}
+          </div>
         </div>
       </div>
     </div>
