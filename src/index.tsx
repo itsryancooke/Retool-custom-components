66fdbdfcd821803816cd5ab2ebb4cb80e3bd4e59
Ryan Cooke <103218097+itsryancooke@users.noreply.github.com>
2026-03-12 17:47:41 +0000

    Adding Circle Positioning Changes
    - Dropdown in inspector for left and right circle positions
    - Conditional positioning

--- src/index.tsx
+++ src/index.tsx
@@ -19,6 +19,13 @@ export const ImageIcon: FC = () => {
     initialValue: "#ff4d4d"
 
   })
+  // Allocates location of circle to corner
+  const circlePosition = Retool.useStateEnumeration({ 
+    name: 'Circle Position',
+    initialValue: 'Right',
+    enumDefinition: ['Left', 'Right'],
+  })
+
   const [count, _setCount] = Retool.useStateNumber({
     name: 'count',
     initialValue: 5
@@ -26,49 +33,56 @@ export const ImageIcon: FC = () => {
   // Click Event
   const clickEvent = Retool.useEventCallback({name: "click"})
 
+
+  // Click Event
+  const clickEvent = Retool.useEventCallback({name: "click"})
+
   // Caps the upper dislay number at 99
   var num = count > 99 ? "99+" : count
   
+  var isRight = (circlePosition[0] === 'Right')
+  
+
   return (
-      <div onClick={() => clickEvent()} style={{
-        position: "relative",
-        height: "100%",        // stretches to container height
-        aspectRatio: "1 / 1", 
+    <div onClick={() => clickEvent()} style={{
+      position: "relative",
+      height: "100%",        // stretches to container height
+      aspectRatio: "1 / 1", 
+      overflow: "hidden",
+    }}>
+      <div style={{
+        position: "absolute",
+        top: 0, left: 0, // defaults to left side
+        width: "100%",
+        height: "100%",
         overflow: "hidden",
       }}>
-        <div style={{
-          position: "absolute",
-          top: 0, left: 0, // defaults to left side
+        <img src={filename} alt="Sample Image" style={{
+          display: "block",
           width: "100%",
           height: "100%",
-          overflow: "hidden",
-        }}>
-          <img src={filename} alt="Sample Image" style={{
-            display: "block",
-            width: "100%",
-            height: "100%",
-            objectFit: "contain",
-          }} />
+          objectFit: "contain",
+        }} />
 
-        <div style={{
-            position: "absolute",
-            top: 0,
-            right: 0,
-            zIndex: 20,
-            width: "25%",
-            aspectRatio: "1 / 1",
-            borderRadius: "50%",
-            backgroundColor: circleColour,
-            color: "white",
-            fontWeight: "bold",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            fontSize: "1rem",
-          }}>
-           {num}
-        </div>
-    </div>
+      <div style={{
+          position: "absolute",
+          top: 0,
+          ...(isRight ? { right: 0 } : { left: 0 }),
+          zIndex: 20,
+          width: "25%",
+          aspectRatio: "1 / 1",
+          borderRadius: "50%",
+          backgroundColor: circleColour,
+          color: "white",
+          fontWeight: "bold",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          fontSize: "1rem",
+        }}>
+         {num}
+      </div>
   </div>
+</div>
   )
 }
